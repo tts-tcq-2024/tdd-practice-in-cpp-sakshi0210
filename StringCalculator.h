bdafcb820623bfd65343a1ecb99615cbe07b0df5
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-30 12:34:28 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,20 @@
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+
+#include <string>
+#include <vector>
+
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+
+private:
+    std::string extractNumbers(const std::string& numbers, std::string& delimiters);
+    std::vector<std::string> split(const std::string& str, const std::string& delimiters);
+    std::vector<int> convertToIntegers(const std::vector<std::string>& tokens);
+    void checkForNegatives(const std::vector<int>& numbers);
+    int calculateSum(const std::vector<int>& numbers);
+};
+
+#endif // STRINGCALCULATOR_H
 
